b1a3f0809101503af4bb9efd29ef15e80a54e499
Nathan Yearout <nathanyearout@gmail.com>
2023-07-31 22:55:47 -0400

    Fix environment variables

--- src/routes/api/main.js
+++ src/routes/api/main.js
@@ -1,4 +1,3 @@
-import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+export const pb = new PocketBase();
